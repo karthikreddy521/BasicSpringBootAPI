9becd73b982ec15db9ec2b565bf33dacbca6af4e
karthikreddy521 <CHINTU@KarthikeddysMBP.fios-router.home>
2019-12-26 22:08:38 -0500

    time based wish code added

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -2,6 +2,9 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 
 @RestController
 @RequestMapping("/hello")
@@ -9,7 +12,26 @@ public class UserController {
 
     @GetMapping("/{name}")
     public String sayHello(@PathVariable final String name){
-        return "Hello.."+name;
+        GregorianCalendar time = new GregorianCalendar();
+        int hour = time.get(Calendar.HOUR_OF_DAY);
+        int min = time.get(Calendar.MINUTE);
+        int day = time.get(Calendar.DAY_OF_MONTH);
+        int month = time.get(Calendar.MONTH) + 1;
+        int year = time.get(Calendar.YEAR);
+
+//        System.out.println("The current time is \t" + hour + ":" + min);
+//        System.out.println("Today's date is \t" + month + "/" + day + "/"
+//                + year);
+
+        if (hour < 12)
+            return "Good Morning!" + name;
+        else if (hour < 17 && !(hour == 12))
+            return "Good afternoon!" + name;
+        else if (hour == 12)
+            return "Good noon!" + name;
+        else
+            return "Good evening!" + name;
+
 
     }
 
